263cd999f9e52f09c467b3f1ceb45e67fcf5348c
Fahim Ahmed <fahim7711@gmail.com>
2026-03-31 17:08:59 +0100

    Added further analysis and statistical tests

--- script/main.R
+++ script/main.R
@@ -3,7 +3,8 @@
 # and Correlation Across Sectors
 
 # Import packages
-install.packages(c("tidyverse", "quantmod", "reshape2", "ggrepel", "viridis"))
+install.packages(c("tidyverse", "quantmod", "reshape2", 
+                   "ggrepel", "viridis", "car", "Hmisc"))
 
 library(tidyverse)
 library(quantmod)
@@ -11,6 +12,8 @@ library(ggplot2)
 library(reshape2)
 library(ggrepel)
 library(viridis)
+library(car)
+library(Hmisc)
 
 # 10 stocks with 3 sectors
 # Apple Inc., Microsoft, NVIDIA for the tech sector
@@ -46,8 +49,10 @@ cumulative_returns <- cumprod(1 + returns)
 
 cumulative_long <- data.frame(date = index(cumulative_returns), 
                               coredata(cumulative_returns)) %>%
-  pivot_longer(-date, names_to = "Stock", values_to = "Value") %>%
+  pivot_longer(cols = -date, names_to = "Stock", values_to = "Value") %>%
   left_join(sector, by = "Stock")
+
+sum(is.na(cumulative_long$Sector))
 
 sector_performance <- cumulative_long %>%
   group_by(date, Sector) %>%
@@ -59,6 +64,33 @@ ggplot(sector_performance, aes(x = date, y = Value, color = Sector)) +
   theme_minimal()
 # Tech sector is showing a relatively steeper line
 
+# Compare two sectors at a time
+unique_sectors <- unique(cumulative_long$Sector)
+
+plot_sector_comparison <- function(data, sectors) {
+  if(length(sectors) != 2) stop("Please provide exactly two sectors")
+  
+  if (!all(sectors %in% data$Sector)) stop("One or more sectors not found")
+  
+  sector_pair <- data %>%
+    filter(Sector %in% sectors) %>%
+    group_by(date, Sector) %>%
+    summarise(Value = mean(Value), .groups = "drop")
+  
+  ggplot(sector_pair, aes(x = date, y = Value, color = Sector)) +
+    geom_line(size = 1.1) +
+    labs(title = paste(sectors[1], " vs ", sectors[2], " Performance"), y = "Growth of £1 investment") +
+    theme_minimal()
+}
+
+# Generate the sector pairs
+sector_pairs <- combn(unique_sectors, 2, simplify = FALSE)
+
+lapply(sector_pairs, function(pair) {
+  print(plot_sector_comparison(cumulative_long, pair))
+})
+
+# Mean returns
 mean_returns <- apply(returns, 2, mean)
 
 summary_table <- data.frame(
@@ -67,15 +99,29 @@ summary_table <- data.frame(
   Avg_Return = mean_returns
 )
 
-summary_table
+summary_table_sorted <- summary_table %>%
+  arrange(desc(Avg_Return))
 
+summary_table_sorted
+
+# ANOVA testing
+anova_result <- aov(Return ~ Sector, data = returns_df)
+summary(anova_result)
+# p-value < 0.05, so sectors are significantly different
+
+# Tukey test
+TukeyHSD(anova_result)
 
 # Section B: Risk and Volatility
 volatility <- apply(returns, 2, sd)
 volatility
 
 summary_table$Volatility <- volatility
-summary_table
+
+summary_table_sorted <- summary_table %>%
+  arrange(desc(Volatility))
+
+summary_table_sorted
 
 returns_df <- data.frame(date = index(returns), coredata(returns)) %>%
   pivot_longer(-date, names_to = "Stock", values_to = "Return") %>%
@@ -86,6 +132,10 @@ ggplot(returns_df, aes(x = Stock, y = Return, fill = Sector)) +
   labs(title = "Daily Returns Distribution by Stock") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Levene's test
+leveneTest(Return ~ Sector, data = returns_df)
+# p < 0.05, so volatility is significantly different across sectors
 
 # Section C: Trends and Market Reactions
 library(zoo)
@@ -104,6 +154,13 @@ ggplot(rolling_avg, aes(x = Date, y = Rolling30, color = Stock)) +
   labs(title = "30-Day Rolling Average Price by Stock", y = "Price") +
   theme_minimal()
 
+# Examine an event like COVID
+returns_df$Period <- ifelse(
+  returns_df$date < "2020-03-01", "Pre-COVID", "Post-Covid"
+  )
+# t-test
+t_test_result <- t.test(Return ~ Period, data = returns_df)
+t_test_result
 
 # Section D: Correlation and Diversification
 corr_matrix <- cor(returns)
@@ -121,3 +178,16 @@ ggplot(corr_df, aes(Var1, Var2, fill = value)) +
   labs(title = "Stock Correlation Matrix") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Test to see correlation significance among stocks
+corr_results <- rcorr(as.matrix(returns))
+corr_results$r
+corr_results$P
+# All pairwise collections were found to be statistically significant (resulting 
+# in values closer to 0), indicating that relationships between stocks are 
+# unlikely due to chance.
+# However, the strength of the relationships varies.
+
+# xts is a time series object, not a dataframe, so the syntax is different
+corr_test <- cor.test(returns[, "AAPL"], returns[, "MSFT"])
+corr_test
